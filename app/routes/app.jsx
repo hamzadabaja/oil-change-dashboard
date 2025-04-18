4f66ddf75faa765d13eb36c5d0f13a205f0599d1
Hamza Dabaja <hamzadabaja24@gmail.com>
2025-04-18 15:31:56 -0400

    Update dashboard UI and metaobject routes

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -19,10 +19,11 @@ export default function App() {
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
       <NavMenu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/inventory">Inventory</Link>
+        <Link to="/app/service">Service</Link>
+        <Link to="/app/invoices">Invoices</Link>
+        <Link to="/app/service-pricing">Service Pricing</Link>
+        <Link to="/app/wholesale">Wholesale</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
